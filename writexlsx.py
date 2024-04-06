4d8767517863297281cd9a0ec7b9f2bf08937296
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2024-04-07 00:54:44 +0500

    googlesheets

--- writexlsx.py
+++ writexlsx.py
@@ -1,6 +1,37 @@
 
 import pandas as pd 
 import os
+import pandas as pd
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+def read_excel_and_replace_sheet():
+    try:
+        # Read data from Excel file into a DataFrame
+        df = pd.read_excel( r"cryptotokens.xlsx")
+        spreadsheet_id = "1nrCJJHTGssyF45m7ujj1BXG2YBLRVHg9_qpMyWiid7I"
+        range_name = "Sheet1"  
+        # Authenticate with Google Sheets API
+        credentials = service_account.Credentials.from_service_account_file(r"key.json", scopes=["https://www.googleapis.com/auth/spreadsheets"])
+        service = build("sheets", "v4", credentials=credentials)
+
+        # Convert DataFrame to values list
+        values_list = df.values.tolist()
+
+        # Write data to the Google Sheets spreadsheet
+        body = {"values": values_list}
+        request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_name, valueInputOption="RAW", body=body)
+        response = request.execute()
+        if response:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f'Error occured in read_excel_replace_sheet')
+        return False
+
+
+
 
 def sort_by_date(df):
     if 'upcoming_launch' in df.columns:
@@ -31,9 +62,13 @@ def write_to_excel(data):
         sorted_df=sort_by_date(updated_df)
 
         sorted_df.to_excel(filename, index=False)
+        read_excel_and_replace_sheet( )
+
 
     else:
         # Write DataFrame to Excel
         new_row.to_excel(filename, index=False)
+        read_excel_and_replace_sheet( )
+
 
 
